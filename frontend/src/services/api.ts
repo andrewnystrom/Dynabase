569472675661e3adfb57cd0c9b59fa7bc273d644
Andrew Nystrom <andrewgnystrom@gmail.com=>
2025-04-27 03:55:31 -0400

    add new logos

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8081', // Your Quarkus backend
+  baseURL: 'http://localhost:8081', // The Quarkus backend
 });
 
 export function setAuthToken(token: string) {
