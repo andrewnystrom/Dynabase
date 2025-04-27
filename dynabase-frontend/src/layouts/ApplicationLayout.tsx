f2f3d6c76e7262807feca95a4b0d00fd99823ed8
Andrew Nystrom <andrewgnystrom@gmail.com=>
2025-04-27 05:40:40 -0400

    remove prime

--- dynabase-frontend/src/layouts/ApplicationLayout.tsx
+++
@@ -1,9 +0,0 @@
-import Navigation from "../components/Navigation"
-export default function ApplicationLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-100 to-indigo-200 p-4">
-      <Navigation />
-      {children}
-    </div>
-  )
-}
